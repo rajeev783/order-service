736b287355e28e9a231659a56a95e205da2b7f95
Rajeev Kumar <rjvkumar783@gmail.com>
2025-04-12 12:21:20 +0530

    added client

--- src/main/java/in/javastack/order/service/OrderService.java
+++ src/main/java/in/javastack/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package in.javastack.order.service;
 
+import in.javastack.order.client.InventoryClient;
 import in.javastack.order.dto.OrderRequestDTO;
 import in.javastack.order.model.Order;
 import in.javastack.order.repo.OrderRepo;
@@ -17,15 +18,20 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepo orderRepo;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequestDTO orderRequestDTO){
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequestDTO.price());
-        order.setQuantity(orderRequestDTO.quantity());
-        order.setSkuCode(orderRequestDTO.skuCode());
-        orderRepo.save(order);
-        log.info("Order placed successfully!");
+        if(inventoryClient.isInStock(orderRequestDTO.skuCode(),orderRequestDTO.quantity())) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequestDTO.price());
+            order.setQuantity(orderRequestDTO.quantity());
+            order.setSkuCode(orderRequestDTO.skuCode());
+            orderRepo.save(order);
+            log.info("Order placed successfully!");
+        } else {
+            throw new RuntimeException("item is out of stock");
+        }
     }
 
     public List<OrderRequestDTO> getAllOrders(){
